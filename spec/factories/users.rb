05cd5c6a734275402c6f11db94ee8eaf42658f18
allezz <aerbay@gmail.com>
2020-04-25 00:48:32 +0200

    Add user model spec file,pass model test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    username { "allezz" }
+    email { "my@string.com" }
+    password { "password123" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+      it { is_expected.to have_db_column :id }
+      it { is_expected.to have_db_column :email }
+      it { is_expected.to have_db_column :username }
+  end
+
+  describe 'Validations' do
+      it { is_expected.to validate_presence_of :username }
+      it { is_expected.to validate_uniqueness_of :username }
+      it { is_expected.to validate_presence_of :email }
+      it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  end
+
+  describe 'factory' do
+      it 'has a valid factory' do
+          expect(create(:user)).to be_valid
+      end
+  end
+
 end
