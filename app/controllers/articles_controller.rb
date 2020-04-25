2aa1444878c1b63cc487eb3f2de1e07da081dfee
allezz <aerbay@gmail.com>
2020-04-25 19:22:59 +0200

    Link user-article,Edit html edit page buttons

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,8 +41,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
- 
-  redirect_to articles_path
+    redirect_to articles_path
   end
   
 end
